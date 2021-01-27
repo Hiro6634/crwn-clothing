dc1228ef98fb723880684ba3a8499b001dccd2d0
suyamaed <suyama_eduardo@redlink.com.ar>
2021-01-27 11:12:16 -0300

    69. Homepage and Directory Components

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+
+import MenuItem from '../menu-item/menu-item.component';
+
+import './directory.styles.css';
+
+class DirectoryMenu extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            sections: [{
+                title: 'hats',
+                imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                id: 1
+            },
+            {
+                title: 'jackets',
+                imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                id: 2
+            },
+            {
+                title: 'snakers',
+                imageUrl: 'https://i.ibb.co/0jqHpnp/snakers.png',
+                id: 3
+            },
+            {
+                title: 'womens',
+                imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                size: 'large',
+                id: 4
+            },
+            {
+                title: 'mens',
+                imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                size: 'large',
+                id: 5
+            }]
+        }
+    }
+
+    render(){
+        return(
+            <div className='directory-menu'>
+                {
+                    this.state.sections.map( section => (
+                        <MenuItem 
+                            key={section.id} 
+                            title={section.title}
+                            imageUrl={section.imageUrl}
+                            size={section.size}
+                        />
+                    ))
+                }
+            </div>
+        );
+    }
+}
+
+export default DirectoryMenu;
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import DirectoryMenu from '../../components/directory/directory.component';
+
+import './homepage.styles.css';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <DirectoryMenu />
+    </div>
+);
+
+export default HomePage;
