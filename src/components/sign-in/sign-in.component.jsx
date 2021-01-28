bd05b2a32d118959291c5f2e87dec3e8aae49b9b
suyamaed <suyama_eduardo@redlink.com.ar>
2021-01-28 00:11:55 -0300

    86. Custom Button Component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 import './sign-in.styles.css';
 
@@ -50,7 +51,7 @@ class SignIn extends Component {
                         required
                     />
 
-                    <input type='submit' value='Submit Form'/>
+                    <CustomButton type='submit'>Sign In</CustomButton>
                 </form>
             </div>
         );
