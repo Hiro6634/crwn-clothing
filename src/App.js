288f44edf090b8a7f4c8a7f8fa7efc81a95d8ca7
Hiro <hiro.suyama@gmail.com>
2021-08-17 15:00:02 -0300

    96. Google Sign In Authentication 2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import React, { Component } from 'react';
 import {Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
@@ -6,17 +6,43 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
-function App() {
-  return (
-    <div>
-      <Header/>
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage}/>
-        <Route path='/signin' component={SignInAndSignUpPage}/>
-      </Switch>
-    </div>
-  );
+import './App.css';
+import { auth } from './firebase/firebase.utils';
+
+class  App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      currentUser: null
+    };
+  }
+ 
+  unsubscribeFromAuth = null;
+
+  componentDidMount(){
+    auth.onAuthStateChanged( user => {
+      this.setState({currentUser: user});
+
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount(){
+    this.unsubscribeFromAuth();
+  }
+  
+  render(){
+    return (
+      <div>
+        <Header/>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage}/>
+          <Route path='/signin' component={SignInAndSignUpPage}/>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
