92547cf82ae2d6af4f9720254bc7bae339f59017
Hiro <hiro.suyama@gmail.com>
2021-08-21 12:04:35 -0300

    104.Storing User Data In Our App

--- src/App.js
+++ src/App.js
@@ -20,11 +20,20 @@ class  App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged( async user => {
-      createUserProfileDocument(user);
-      //this.setState({currentUser: user});
+    this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+              currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+      }
+      this.setState({ currentUser: userAuth});
     });
   }
 
