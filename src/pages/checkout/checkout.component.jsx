64fd45e2e5ece5b2e70fe7d4fd5fe9d41a095385
Hiro <hiro.suyama@gmail.com>
2021-08-28 00:33:52 -0300

    137. Dispatch Action Shorthand

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -32,7 +32,7 @@ const CheckoutPage = ({cartItems, total}) => (
        } 
        <div className='total'>
            <span>TOTAL: ${total}</span>
-        </div>
+         </div>
     </div>
 );
 
