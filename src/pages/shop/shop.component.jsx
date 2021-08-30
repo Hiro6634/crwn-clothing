3ff80a53518671ba21d5a4549ea2ac367686c6b2
Hiro <hiro.suyama@gmail.com>
2021-08-29 22:57:45 -0300

    145.Collection Overview Component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,24 +1,14 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux'; 
-import { createStructuredSelector } from 'reselect'; 
+import React from 'react';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-import { selectCollections } from '../../redux/shop/shop.selectors'; 
+import CollectionOverview from '../../components/collections-overview/collections-overview.component';
 
 import './shop.styles.scss';
 
 const ShopPage = ({collections}) => (
     <div className='shop-page'>
-        {
-            collections.map( ({id, ...otherCollectionProps}) => (
-                <CollectionPreview key={id}{...otherCollectionProps}/>
-            ))
-        }
+        <CollectionOverview/>
     </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
-});
 
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,11 +1,10 @@
-import { bindActionCreators } from 'redux';
 import SHOP_DATA from './shop.data';
 
 const INITIAL_STATE ={
     collections: SHOP_DATA
 };
 
-const shopReducer = (state=INITIAL_STATE, action) =>{
+const shopReducer = (state=INITIAL_STATE, action) => {
     switch(action.type){
         default:
             return state;
