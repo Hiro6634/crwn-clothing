b40a2ba8746a380bf19752e7f719154775f7ff84
Hiro <hiro.suyama@gmail.com>
2021-08-30 08:35:13 -0300

    147.Improvihg Naming Of Component

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import collectionItem from '../../components/collection-item/collection-item.component';
+
+import './collection.styles.scss';
+
+const CollectionPage = () => (
+    <div className='collection-page'>
+        <h2>COLLECTION PAGE</h2>
+    </div>
+);
+
+export default CollectionPage;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import CollectionOverview from '../../components/collections-overview/collections-overview.component';
-import CategoryPage from '../category/category.component';
+import CollectionPage from '../collection/collection.component';
 
 import './shop.styles.scss';
 
 const ShopPage = ({match}) => (
     <div className='shop-page'>
         <Route exact path={`${match.path}`} component={CollectionOverview} /> 
-        <Route path={`${match.path}/:categoryId`} component={CategoryPage}/>  
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>  
 
     </div>
 );
