185ac2b9318a60b8862c3249f92758847f85105b
Eduardo Suyama <SUYAMAED@redlink.com.ar>
2021-11-20 15:05:24 -0300

    68.E-commerce Homepage + SASS Setup

--- src/redux/shop/shop.actions.js
+++
@@ -1,6 +0,0 @@
-import ShopActionsTypes from "./shop.types";
- 
-export const updateCollections = (collectionsMap) => ({
-    type: ShopActionsTypes.UPDATE_COLLECTIONS,
-    payload: collectionsMap
-});
