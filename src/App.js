cb327b5466a58e64816d5cd476fd3b59fd522d56
Eduardo Suyama <SUYAMAED@redlink.com.ar>
2021-11-22 01:08:57 -0300

    80. withRouter()

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
-function App(){
-  return <div>
-    <HomePage/>
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
   </div>
+);
+
+function App(){
+  return( 
+  <div>
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route path='/hats' component={HatsPage} />    </Switch>
+  </div>
+  );
 }
 
 export default App;
