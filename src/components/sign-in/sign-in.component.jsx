00ad60b7e9243f15c6b8045d7d4736933d8c6820
Eduardo Suyama <SUYAMAED@redlink.com.ar>
2021-11-25 09:51:05 -0300

    109. Sign In With Email And Password plus

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -56,7 +56,7 @@ class SignIn extends React.Component {
                         required />
                     <div className='buttons'>
                         <CustomButton type="submit">SIGN IN</CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>SIGN IN WITH GOOGLE</CustomButton>
+                        <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>SIGN IN WITH GOOGLE</CustomButton>
                     </div>
                 </form> 
             </div>
