02cfffee4e5a1888a1052589143c1a359a298190
Eduardo Suyama <SUYAMAED@redlink.com.ar>
2021-11-26 16:24:30 -0300

    124. Implementing Redux in Cart

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,8 +8,9 @@ import './header.styles.scss';
 
 import { auth } from "../../firebase/firebase.utils";
 import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
     <div className='header'>
         <Link to='/'>
             <Logo className='logo-container'/>
@@ -33,11 +34,19 @@ const Header = ({currentUser}) => (
             }
             <CartIcon/>
         </div> 
+        {
+            hidden ? (
+                null
+            ) : (
+                <CartDropdown/>
+            )
+        }
     </div>
 );
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
-})
+const mapStateToProps = ({user: {currentUser}, cart: { hidden}})  => ({
+    currentUser,
+    hidden
+});
 
 export default connect(mapStateToProps)( Header);
