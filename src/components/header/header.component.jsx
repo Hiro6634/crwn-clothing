c6920478081ceec7d3b9623d9b9010f8f151994b
Eduardo Suyama <SUYAMAED@redlink.com.ar>
2021-11-26 10:43:57 -0300

    122. Cart Component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,6 +7,7 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
 
 import { auth } from "../../firebase/firebase.utils";
+import CartIcon from "../cart-icon/cart-icon.component";
 
 const Header = ({currentUser}) => (
     <div className='header'>
@@ -30,6 +31,7 @@ const Header = ({currentUser}) => (
                         SIGN IN
                     </Link>
             }
+            <CartIcon/>
         </div> 
     </div>
 );
