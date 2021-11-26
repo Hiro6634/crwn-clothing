2554bee222f90162415677da63bc91fa3dafbcd6
Eduardo Suyama <SUYAMAED@redlink.com.ar>
2021-11-26 01:09:03 -0300

    120.mapDispatchToProps

--- src/App.js
+++ src/App.js
@@ -7,34 +7,29 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { connect } from 'react-redux';
+import { setCurrentUser } from './redux/user/user.actions';
+
+import './App.css';
 
 class App extends React.Component {
-  constructor(){
-    super();
-
-    this.state = {
-      currentUser: null
-    }
-  }
-
   unsubscribeFromAuth = null;
 
   componentDidMount(){
+    const {setCurrentUser} = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
       if(userAuth){
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
+          setCurrentUser({
               id: snapShot.id,
               ...snapShot.data()
-            }
           });
         });
       }
       else{
-        this.setState({ currentUser: userAuth});
+        setCurrentUser(userAuth);
       }
     });
   }
@@ -56,4 +51,7 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser:  user => dispatch(setCurrentUser(user))
+});
+export default connect(null, mapDispatchToProps)(App);
