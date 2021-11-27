7b446f45e0129f51437ce55ad0fdaab848c713ae
Eduardo Suyama <SUYAMAED@redlink.com.ar>
2021-11-27 10:08:53 -0300

    133. Reselect Library

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 
 import {ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 
 import './cart-icon.styles.scss';
 
@@ -17,9 +18,8 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-const mapStateToProps = ({cart: {cartItems}}) => ({
-    itemCount: cartItems.reduce(
-        (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,0 )
+const mapStateToProps = state => ({
+    itemCount: selectCartItemsCount(state)
 });
 
 export default connect(
