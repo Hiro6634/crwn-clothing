d82a0609a25839ab1042ee0fcabb27647c74f47d
Eduardo Suyama <SUYAMAED@redlink.com.ar>
2021-11-29 17:24:20 -0300

    141. Remove Items From Cart

---
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import { clearItemFromCart } from "../../redux/cart/cart.actions";
+
+import './checkout-item.styles.scss';
+const CheckoutItem = ({cartItem, clearItem}) => {
+    const {name,imageUrl, quantity, price} = cartItem;
+    return(
+        <div className='checkout-item'>
+            <div className='image-container'>
+                <img src={imageUrl} alt='item' />
+            </div>
+            <span className='name'>{name}</span>
+            <span className='quantity'>{quantity}</span>
+            <span className='price'>{price * quantity}</span>
+            <span className='remove-button' onClick={() => clearItem(cartItem)}>&#10005;</span>
+        </div>
+    );  
+};
+
+const mapDispatchToProps = dispatch => ({
+    clearItem: item => dispatch(clearItemFromCart(item))
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
