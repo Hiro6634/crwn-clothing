037eed3841eb24f89343ebbf97a15d7f5699f75f
Eduardo Suyama <SUYAMAED@redlink.com.ar>
2021-12-02 00:07:05 -0300

    153 Data Normalization + Collection Page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -9,7 +9,7 @@ import './shop.styles.scss'
 const ShopPage = ({match}) => (
     <div className='shop-page'>
         <Route exact path={`${match.path}`} component={CollectionsOverview}/>
-        <Route path={`${match.path}/:categoryId`} component={CollectionPage}/>
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
     </div>
 );
 
