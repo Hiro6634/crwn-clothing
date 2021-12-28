54ddd76c00186a3ec97ba350405e486bcf57aae0
Eduardo Suyama <SUYAMAED@redlink.com.ar>
2021-12-27 23:33:14 -0300

    185. WithSpinner HOC 2

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -3,12 +3,20 @@ import { Route } from "react-router-dom";
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from "../collection/collection.component";
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
 
 import { firestore, convertCollectionSnapshotToMap } from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
 import { updateCollections } from '../../redux/shop/shop.actions';
 
+const CollectionOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
 class ShopPage extends React.Component {
+    state = {
+        loading: TextTrackCue
+    };
+
     unsubscribeFromSnapshot = null;
 
     componentDidMount(){
@@ -18,17 +26,19 @@ class ShopPage extends React.Component {
         collectionRef.onSnapshot(async snapshot => {
            const collectionsMap = convertCollectionSnapshotToMap(snapshot);
            updateCollections(collectionsMap);
+           this.setState({loading: false});
         });
     }
 
     render(){
         const {match} = this.props;
+        const {loading} = this.state;
         return(
             <div className='shop-page'>
-                <Route exact path={`${match.path}`} component={CollectionsOverview}/>
-                <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
+                <Route exact path={`${match.path}`} render={(props) => <CollectionOverviewWithSpinner isLoading={loading}{...props}/>}/>
+                <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={loading}{...props}/>}/>
             </div>
-        );
+        );  
     }
 };
 
