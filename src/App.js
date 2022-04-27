b6f01f5be07158adac873f282dc685b36dd0cb59
Eduardo Suyama <SUYAMAED@redlink.com.ar>
2022-04-27 08:41:59 -0300

    React Router Outlet

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
 
+const ShopPage = () => (
+  <h1>I'm Shop Page</h1>
+);
+
 const  App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home/>}/>
+      <Route path='/home' element={<Home/>}>
+        <Route path='shop' element={<ShopPage/>}/>
+    </Route>
     </Routes>
     );
 }
