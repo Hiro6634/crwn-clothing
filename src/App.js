9e77e3416c1601b4261880cec7fa30e88f6c11ff
Eduardo Suyama <SUYAMAED@redlink.com.ar>
2022-04-27 08:57:29 -0300

    React Router Link

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
-import { Routes, Route, Outlet } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
-
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>I'm the navigation bar</h1>
-      </div>
-      <Outlet/>>
-    </div>
-  )
-}
+import Navigation from './routes/navigation/navigation.component';
 
 const Shop = () => (
   <h1>I'm the Shop Page</h1>
