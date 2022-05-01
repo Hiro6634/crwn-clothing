e8471bbf152918b14cc2541108c16fc13e082c9e
Eduardo Suyama <SUYAMAED@redlink.com.ar>
2022-05-01 18:32:58 -0300

    Custom Button Component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react';
 
 import { createAuthUserWithEmailAndPassword, createUserDocuemntFromAuth } from '../../utils/firebase/firebase.utils';
 import FormInput from '../form-input/form-input.component';
+import Button from '../button/button.component';
+
+import './sign-up-form.styles.scss';
 
 const defaultFormFields = {
     displayName: '',
@@ -50,8 +53,9 @@ const SignUpForm = () => {
     }
 
     return(
-        <div>
-            <h1>Sign up with email and password</h1>
+        <div className='sign-up-container'>
+            <h2>Don't have an account</h2>
+            <span>Sign up with email and password</span>
             <form onSubmit={handleSubmit}>
                 <FormInput 
                     label='Display Name'
@@ -89,7 +93,7 @@ const SignUpForm = () => {
                     required
                 />
                 
-                <button type='submit'>Sign Up</button>
+                <Button type='submit'>Sign Up</Button>
             </form>
         </div>
     );
