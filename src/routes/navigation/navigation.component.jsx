eca107ad555daf8299a1db1f77e9c6de78ac1673
Hiro Suyama <hiro.suyama@gamil.com>
2022-05-05 18:16:29 -0300

    Cart Icon and Dropdown

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,6 +1,9 @@
 import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom'
 
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { UserContext } from '../../context/user.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
@@ -29,9 +32,10 @@ const Navigation = () => {
                 <Link className='nav-link' to='/auth'>
                   SIGN IN
                 </Link>
-              )
-            }
+            )}
+            <CartIcon/>
           </div>
+          <CartDropdown/>
       </div>
       <Outlet/>
     </Fragment>
