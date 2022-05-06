9f75fea61b1422750df98ec035d0c558be23bdf5
Hiro Suyama <hiro.suyama@gamil.com>
2022-05-06 12:32:36 -0300

    Decrement Checkout Item Quantity

--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -15,7 +15,26 @@ const addCartItem = (cartItems, productToAdd) => {
     }
 
     return [...cartItems, {...productToAdd, quantity: 1}];
-}
+};
+
+const removeCartItem = (cartItems, cartItemToRemove) => {
+    // finde the cart item to remove
+    const existingCartItem = cartItems.find(
+        (cartItem) => cartItem.id === cartItemToRemove.id
+    );
+
+    //check if quantity is equal to 1, if it is remove that item from cart
+    if(existingCartItem.quantity === 1){
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);               
+    }
+
+    //return nack cartitems with matching cart item with reduced quantity
+    return cartItems.map((cartItem)=>
+        cartItem.id===cartItemToRemove.id
+        ? {...cartItem, quantity: cartItem.quantity - 1}
+        : cartItem 
+    );
+};
 
 export const CartContext = createContext({
     isCartOpen: false,
@@ -34,11 +53,22 @@ export const CartProvider = ({children}) => {
         const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity,0);
         setCartCount(newCartCount);
     },[cartItems]);
+   
     const addItemToCart = (productToAdd) => {
         setCartItems(addCartItem(cartItems, productToAdd));
     }
 
-    const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount };
+    const removeItemToCart = (cartItemToRemove) => {
+        setCartItems(removeCartItem(cartItems, cartItemToRemove));
+    }
+    const value = {
+        isCartOpen, 
+        setIsCartOpen, 
+        addItemToCart, 
+        removeItemToCart,
+        cartItems, 
+        cartCount 
+    };
 
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
