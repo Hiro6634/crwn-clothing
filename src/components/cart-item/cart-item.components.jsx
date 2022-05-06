d3fbd61b0e7ef66b0a924a243930e915ae2d9ea5
Hiro Suyama <hiro.suyama@gamil.com>
2022-05-06 10:40:32 -0300

    Cart Items Design

--- src/components/cart-item/cart-item.components.jsx
+++ src/components/cart-item/cart-item.components.jsx
@@ -1,13 +1,18 @@
 import './cart-item.styles.scss';
 
 const CartItem = ({cartItem}) => {
-    const {name, quantity} = cartItem;
+    const {name, imageUrl, price, quantity} = cartItem;
     return(
         <div className='cart-item-container'>
-            <h2>{name}</h2>
-            <span>{quantity}</span>
+            <img src={imageUrl} alt={`${name}`}/>
+            <div className='item-details'>
+                <span className='name'>{name}</span>
+                <span className='price'>
+                    {quantity} x ${price}
+                </span>
+            </div>
         </div>
     );
-}
+};
 
 export default CartItem;
--- src/context/products.context.jsx
+++ src/context/products.context.jsx
@@ -5,7 +5,7 @@ import PRODUCTS from '../shop-data.json';
 export const ProductContext = createContext({});
 
 export const ProductsProvider = ({children}) =>  {
-    const [products, setProducts] = useState(PRODUCTS);
+    const [products] = useState(PRODUCTS);
     const value = { products };
     return(
         <ProductContext.Provider value={value}> {children} </ProductContext.Provider>
