ccffa21af2ef339ca109458aeb78bcc9750cf182
Hiro Suyama <hiro.suyama@gamil.com>
2022-05-05 23:06:39 -0300

    Toggle Cart Open

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { UserProvider } from './context/user.context';
 import { ProductsProvider } from './context/products.context';
+import { CartProvider } from './context/cart.context';
 
 import reportWebVitals from './reportWebVitals';
 
@@ -14,11 +15,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <ProductsProvider>
-        <UserProvider>
-          <App />
-        </UserProvider>
-      </ProductsProvider>
+      <UserProvider>
+        <ProductsProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </ProductsProvider>
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,14 +4,17 @@ import { Outlet, Link } from 'react-router-dom'
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
-import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { UserContext } from '../../context/user.context';
+import { CartContext } from '../../context/cart.context';
+
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
 import './navigation.styles.scss';
 
 const Navigation = () => {
-  const { currentUser, setCurrentUser } = useContext(UserContext);
+  const { currentUser } = useContext(UserContext);
+  const { isCartOpen} = useContext(CartContext);
 
   return (
     <Fragment>
@@ -35,7 +38,7 @@ const Navigation = () => {
             )}
             <CartIcon/>
           </div>
-          <CartDropdown/>
+          {isCartOpen && <CartDropdown/>}
       </div>
       <Outlet/>
     </Fragment>
