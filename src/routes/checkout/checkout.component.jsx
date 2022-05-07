3aecb49df88741d57b54f6024edfc654ebbefb65
Hiro Suyama <hiro.suyama@gamil.com>
2022-05-07 12:13:54 -0300

    Cart Total

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -6,7 +6,7 @@ import CheckoutItem from '../../components/checkout-item/checkout-item.component
 import './checkout.styles.scss';
 
 const Checkout = () => {
-    const { cartItems } = useContext(CartContext);
+    const { cartItems, cartTotal } = useContext(CartContext);
     return(
         <div className='checkout-container'>
             <div className='checkout-header'>
@@ -29,7 +29,7 @@ const Checkout = () => {
             {cartItems.map((cartItem) => (
                 <CheckoutItem key={cartItem.id} cartItem={cartItem}/>
             ))}
-            <div className='total'>Total: 0</div>
+            <div className='total'>Total: ${cartTotal}</div>
         </div>
     );
 };
