fef0e7bc75c4cc0289c16060f870cd3988588026
Hiro Suyama <hiro.suyama@gamil.com>
2022-05-08 13:31:52 -0300

    Styled-Components Button

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -5,7 +5,7 @@ import {
     signInAuthWithEmailAndPassword
 } from '../../utils/firebase/firebase.utils';
 import FormInput from '../form-input/form-input.component';
-import Button from '../button/button.component';
+import Button, {BUTTON_TYPE_CLASSES} from '../button/button.component';
 
 import './sign-in-form.styles.scss';
 
@@ -77,7 +77,7 @@ const SignInForm = () => {
                
                 <div className='buttons-container'>
                     <Button type='submit'>Sign </Button>
-                    <Button type='button' buttonType='google' onClick={signInWithGoogle}>Google sign in</Button>
+                    <Button type='button' buttonType={BUTTON_TYPE_CLASSES.google} onClick={signInWithGoogle}>Google sign in</Button>
                 </div>
             </form>
         </div>
