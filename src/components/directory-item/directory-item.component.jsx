968c4882fb1e0c40d0a54825d4c78cf65374f2ab
Hiro Suyama <hiro.suyama@gamil.com>
2022-05-08 18:56:23 -0300

    Styled-Components Directory Item & Cart Icon

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,18 +1,22 @@
-import './directory-item.styles.scss';
+import {
+  BackgroundImage,
+  Body,
+  DirectoryItemContainer
+} from './directory-item.styles';
 
 const DirectoryItem = ({category}) => {
     const { imageUrl, title} = category;
 
     return(
-        <div className='directory-item-container'>
-          <div className='background-image' style={{
-            backgroundImage: `url(${imageUrl})`
-            }}/>
-          <div className='body'>
-            <h2>{title}</h2>
+        <DirectoryItemContainer>
+          <BackgroundImage 
+          imageUrl={imageUrl}
+          />
+          <Body>
+            <h2>{title.toUpperCase()}</h2>
             <p>Shop Now</p>
-          </div>
-        </div>
+          </Body>
+        </DirectoryItemContainer>
     );
 };
 
