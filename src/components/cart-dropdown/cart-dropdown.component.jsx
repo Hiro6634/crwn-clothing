c17f0682223550699b18a8d50099d361a73c1141
Hiro Suyama <hiro.suyama@gamil.com>
2022-05-08 18:23:38 -0300

    Styled-Components Dropdown-component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -7,7 +7,11 @@ import Button from '../button/button.component';
 
 import CartItem from '../cart-item/cart-item.components';
 
-import './cart-dropdown.styles.scss';
+import{
+    CartDropdownContainer,
+    EmptyMessage,
+    CartItems
+} from './cart-dropdown.styles';
 
 const CartDropdown = () => {
     const { cartItems } = useContext(CartContext);
@@ -18,14 +22,18 @@ const CartDropdown = () => {
     };
     
     return(
-        <div className='cart-dropdown-container'>
-            <div className='cart-items'>
+        <CartDropdownContainer>
+            <CartItems>
             {
-                cartItems.map( item => <CartItem key={item.id} cartItem={item}/>)
+                cartItems.length ? (cartItems.map( item => (
+                    <CartItem key={item.id} cartItem={item}/>
+                ))):(
+                    <EmptyMessage>Your cart is empty</EmptyMessage>
+                )
             }
-            </div>
+            </CartItems>
             <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
-        </div>
+        </CartDropdownContainer>
     )
 }
 
