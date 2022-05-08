78f5742095e5350dc7d82200e7efcdad06a95337
Hiro Suyama <hiro.suyama@gamil.com>
2022-05-08 19:37:00 -0300

    Last Touches

--- src/components/directory/directory.styles.jsx
+++ src/components/directory/directory.styles.jsx
@@ -1,8 +1,11 @@
-.directory-container{
+import styled from 'styled-components';
+
+
+export const DirectoryContainer = styled.div`
     width: 100%;
     display: flex;
     flex-wrap: wrap;
     justify-content: space-between;
-}
+`;
 
 
