658dcbe742701302486c329efa8f2f3d8812e2e7
Hiro Suyama <hiro.suyama@gamil.com>
2022-05-09 23:24:23 -0300

    UserReducer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
 
+
 const  App = () => {
   return (
     <Routes>
