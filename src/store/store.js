380fa10af2ad66a631596717a3cb7e0ce9ab158d
Hiro Suyama <hiro.suyama@gamil.com>
2022-05-14 13:46:15 -0300

    Redux-Persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,7 @@
 import {compose, createStore, applyMiddleware} from 'redux';
+import { persistStore, persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 // import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
@@ -16,8 +19,22 @@ const loggerMiddleware = (store) => (next) => (action) => {
     console.log('next state: ', store.getState());
 }
 
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const middleWares = [loggerMiddleware];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(
+    persistedReducer, 
+    undefined, 
+    composedEnhancers
+);
+
+export const persistor = persistStore(store);
