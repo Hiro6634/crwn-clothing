06261ce4d81f6424dd055e0b36de2d42fea0840e
Hiro Suyama <hiro.suyama@gamil.com>
2022-05-15 16:08:38 -0300

    Redux-Saga: Check Usere Session Saga

--- src/App.js
+++ src/App.js
@@ -2,24 +2,18 @@ import { Routes, Route } from 'react-router-dom';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 
-import { 
-    onAuthStateChangedListener,
-    createUserDocuemntFromAuth,
-    getCurrentUSer
-} from './utils/firebase/firebase.utils';
-
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
 
-import { setCurrentUser } from './store/user/user.action';
+import { checkUserSession } from './store/user/user.action';
 
 const  App = () => {
   const dispatch = useDispatch();
   useEffect(()=>{
-    getCurrentUSer().then((user)=>console.log(user));
+    dispatch(checkUserSession());
 },[]);
 
   return (
