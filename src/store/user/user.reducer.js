7f232974219aeafef12a2f189bc4763fe6cf0275
Hiro Suyama <hiro.suyama@gamil.com>
2022-05-16 00:29:29 -0300

    Redux-Saga: Check User Session Saga

--- src/store/user/user.reducer.js
+++ src/store/user/user.reducer.js
@@ -7,11 +7,10 @@ const INITIAL_STATE = {
 export const userReducer = (state=INITIAL_STATE, action) => {
     const {type, payload} = action;
     switch(type) {
-        case USER_ACTION_TYPES.SET_CURRENT_USER:
-            return {
-                ...state,
-                currentUser: payload
-            };
+        case USER_ACTION_TYPES.SIGN_IN_SUCCESS:
+            return { ...state, currentUser: payload };
+        case USER_ACTION_TYPES.SIGN_IN_FAILED:
+            return { ...state, error: payload };
         default:
             return state;
     }
