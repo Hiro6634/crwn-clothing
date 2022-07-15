5790b6f495432eddd0ab8235c299469a0fa34cf5
Hiro Suyama <hiro.suyama@gamil.com>
2022-07-14 23:58:41 -0300

    Improving Our Reducer Typing

--- src/store/categories/category.action.ts
+++ src/store/categories/category.action.ts
@@ -1,32 +1,35 @@
-import { CATEGORIES_ACTION_TYPES, Category, CatergoryItem } from "./categories.types";
+import { CATEGORIES_ACTION_TYPES, Category } from "./categories.types";
 
-import { createAction, Action, ActionWithPayload } from "../../utils/reducer/reducer.utils";
+import { 
+    createAction, 
+    Action, 
+    ActionWithPayload,
+    withMatcher
+} from "../../utils/reducer/reducer.utils";
 
 export type FetchCategoriesStart = 
     Action<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START>;
 
-export type FetchCategoriesSuccess = 
-    ActionWithPayload<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, 
+export type FetchCategoriesSuccess = ActionWithPayload<
+    CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, 
     Category[]
 >;
 
-export type FetchCategoriesFailed = 
-    ActionWithPayload<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, 
+export type FetchCategoriesFailed = ActionWithPayload<
+    CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, 
     Error
 >;
 
-export type CategoryAction = 
-    | FetchCategoriesStart 
-    | FetchCategoriesSuccess 
-    | FetchCategoriesFailed;
+export const fetchCategoriesStart = withMatcher(
+    (): FetchCategoriesStart =>
+    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START));
 
-export const fetchCategoriesStart = () =>
-    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
+export const fetchCategoriesSuccess = withMatcher(
+    (categoriesArray: Category[]): FetchCategoriesSuccess =>
+    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, categoriesArray));
 
-export const fetchCategoriesSuccess = (categoriesArray) =>
-    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, categoriesArray);
-
-export const fetchCategoriesFailed = (error) =>
-    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
+export const fetchCategoriesFailed = withMatcher(
+    (error: Error): FetchCategoriesFailed =>
+    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error));
 
 
--- src/store/categories/category.reducer.ts
+++ src/store/categories/category.reducer.ts
@@ -1,7 +1,20 @@
-import { CATEGORIES_ACTION_TYPES } from "./categories.types";
+import {  Category} from "./categories.types";
 
-import { CategoryAction } from "./category.action";
-export const CATEGORIES_INITIAL_STATE = {
+import { AnyAction } from "redux";
+
+import { 
+    fetchCategoriesStart, 
+    fetchCategoriesSuccess, 
+    fetchCategoriesFailed 
+} from "./category.action";
+
+export type CategoriesState = {
+    readonly categories: Category[];
+    readonly isLoading: boolean;
+    readonly error: Error | null;
+};
+
+export const CATEGORIES_INITIAL_STATE: CategoriesState = {
     categories: [],
     isLoading: false,
     error: null,
@@ -9,29 +22,19 @@ export const CATEGORIES_INITIAL_STATE = {
 
 export const categoriesReducer = (
     state = CATEGORIES_INITIAL_STATE, 
-    action = {} as CategoryAction
-) => {
-    const {type, payload} = action;
-
-    switch(type){
-        case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
-            return {
-                ...state,
-                isLoading: true
-            };
-        case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
-            return {
-                ...state,
-                error: payload,
-                isLoading: false,
-            }
-        case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
-            return{
-                ...state,
-                categories: payload,
-                isLoading: false,
-            };
-        default:
-            return state;
+    action = {} as AnyAction
+): CategoriesState => {
+    if(fetchCategoriesStart.match(action)){
+        return {...state, isLoading: true};
     }
+
+    if(fetchCategoriesSuccess.match(action)){
+        return {...state, categories: action.payload, isLoading: false};
+    }
+
+    if(fetchCategoriesFailed.match(action)){
+        return {...state, error: action.payload, isLoading: false};
+    }
+
+    return state;
 };
